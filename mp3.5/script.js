923d6c89c212a1075b0d8b6197952f93ccb88a47
mehdi rezaei arshad <mehdirezaeiarshad@gmail.com>
2025-01-29 23:46:51 +0330

    little bit more js and it will be done...

---
+++ mp3.5/script.js
@@ -0,0 +1,7 @@
+const playerAlbumImage = document.getElementById("album-image")
+const playerAlbumName = document.getElementById("album-name");
+const previousSongButton = document.getElementById("previous-song-button");
+const playSongButton = document.getElementById("pause-song-button");
+const nextSongButton = document.getElementById("next-song-button");
+const shuffleSongButton = document.getElementById("shuffle-song-button");
+const playlistFieldset = document.getElementById("playlist-fieldset");
